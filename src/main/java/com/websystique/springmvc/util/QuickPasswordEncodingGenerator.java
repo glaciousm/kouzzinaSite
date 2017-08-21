a2673c985c55023253d35dfabe4714315179b195
Menelaos Mamouzellos <menelaos.mamouzellos@iteam.gr>
2017-08-21 12:04:10 +0300

    Initial Commit

---
+++ src/main/java/com/websystique/springmvc/util/QuickPasswordEncodingGenerator.java
@@ -0,0 +1,13 @@
+package com.websystique.springmvc.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class QuickPasswordEncodingGenerator {
+
+    public static void main(String[] args) {
+            String password = "12345";
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            System.out.println(passwordEncoder.encode(password));
+    }
+  
+}
